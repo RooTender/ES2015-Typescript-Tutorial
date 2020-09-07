d05411d103cc0947a1eef6c1c7b8002285bf8798
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2020-09-07 16:02:15 +0200

    Update tutorial6.ts

--- tutorial6.ts
+++ tutorial6.ts
@@ -1,19 +1,8 @@
-function greetPerson(name){
-     let greet;
-    if(name === "Chandler"){
-       let greet = "Hello Chandler";
-    }else{
-      let  greet = "Hi there";
-    }
-    console.log(greet);
-   
-}
-greetPerson("Chandler");
+var a = 1;
+var b = 2;
 
-var a=1;
-var b=2;
-if(a === 1){
-    var a=10;
+if(a === 1) {
+    var a = 10;
     let b = 20;
     console.log(a); //10
     console.log(b); //20
@@ -21,8 +10,7 @@ if(a === 1){
 console.log(a); //10
 console.log(b); //2
 
-var c=1;
-var c=2;
-
-let d=1;
-//let d=2;
+/*
+     var has global scope
+     let has local scope
+*/
